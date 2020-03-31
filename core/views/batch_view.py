5041e4104e18a606838195bfa5d2e1ee43eb7d9d
Angel <alm.angel97@gmail.com>
2020-03-31 14:03:40 +0200

    trying to set up batch upstream endpoint

--- core/views/batch_view.py
+++ core/views/batch_view.py
@@ -1,3 +1,9 @@
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from core.models.hello import Hello
+from rest_framework.decorators import api_view
 from core.serializers.batch_serializer import BatchSerializer
 
 @api_view(['POST'])
